d7769492eacde087f0a3080f8c33abbb8527457a
mohamed224 <mdiaby00@gmail.com>
2022-11-01 20:32:21 +0100

    init project

---
+++ src/main/java/com/squadteam/SpringbootGraphqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.squadteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraphqlExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squadteam/SpringbootGraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
